a5228608a8a0be430a156bd3d6d6f4b57cf37892
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-28 05:11:06 +0700

    Create ArisEnderChest.java

---
+++ src/main/java/me/aris/arisenderchest/ArisEnderChest.java
@@ -0,0 +1,33 @@
+package me.aris.arisenderchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import me.aris.arisenderchest.commands.ECCommand;
+import me.aris.arisenderchest.listeners.InventoryListener;
+import me.aris.arisenderchest.managers.DataManager;
+
+public class ArisEnderChest extends JavaPlugin {
+    private static ArisEnderChest instance;
+    private DataManager dataManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        this.dataManager = new DataManager();
+        getCommand("ec").setExecutor(new ECCommand());
+        getServer().getPluginManager().registerEvents(new InventoryListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        dataManager.saveAll();
+    }
+
+    public static ArisEnderChest getInstance() {
+        return instance;
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+}

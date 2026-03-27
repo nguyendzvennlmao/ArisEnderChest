3df8a5edcc780c27afcfbd7a4840fc06eb03f08e
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-28 05:39:22 +0700

    Create ECCommand.java

---
+++ src/main/java/me/aris/arisenderchest/commands/ECCommand.java
@@ -0,0 +1,40 @@
+package me.aris.arisenderchest.commands;
+
+import me.aris.arisenderchest.ArisEnderChest;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class ECCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            if (!player.hasPermission("arisenderchest.use")) return true;
+            int rows = getRows(player);
+            Inventory ec = ArisEnderChest.getInstance().getDataManager().getEnderChest(player, rows);
+            player.openInventory(ec);
+        } else {
+            if (!player.hasPermission("arisenderchest.admin")) return true;
+            @SuppressWarnings("deprecation")
+            OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+            Inventory ec = ArisEnderChest.getInstance().getDataManager().getEnderChest(target, 6);
+            player.openInventory(ec);
+        }
+        return true;
+    }
+
+    private int getRows(Player player) {
+        if (player.hasPermission("arisenderchest.row.6")) return 6;
+        if (player.hasPermission("arisenderchest.row.5")) return 5;
+        if (player.hasPermission("arisenderchest.row.4")) return 4;
+        return 3;
+    }
+                                      }
